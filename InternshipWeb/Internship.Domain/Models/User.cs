d17d74d887c836293fe297d0a3263564b313e554
KuroCoder <habib.pa98@gmail.com>
2020-08-31 16:51:40 +0430

    Create User Model

---
+++ InternshipWeb/Internship.Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Internship.Domain.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
