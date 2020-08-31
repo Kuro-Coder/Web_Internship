cbd0a46f9133b7dd9123270757a4ab49b9a5ec2d
KuroCoder <habib.pa98@gmail.com>
2020-08-31 17:48:18 +0430

    Add UserRole Table

---
+++ InternshipWeb/Internship.Domain/Models/UserRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Internship.Domain.Models
+{
+    public class UserRole
+    {
+        [Key]
+        public int UserRoleId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int RoleId { get; set; }
+
+        #region Relations
+
+
+
+        #endregion
+    }
+}
