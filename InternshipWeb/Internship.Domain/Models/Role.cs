c7fe26f3b829940290fd8818c24f5dda0e54e283
KuroCoder <habib.pa98@gmail.com>
2020-08-31 17:45:39 +0430

    Add Role Table

---
+++ InternshipWeb/Internship.Domain/Models/Role.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Internship.Domain.Models
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Display(Name = "نقش کابری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید!!!")]
+        [MaxLength(100, ErrorMessage = "{0} نمیتواند بیشتر از {1} کاراکتر باشد!!!")]
+        public string RoleName { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        #region Relations
+
+
+
+        #endregion
+
+    }
+}
