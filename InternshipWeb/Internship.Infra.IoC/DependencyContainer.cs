af568471388d8ada2d7a199f0a44c69764bdb7f9
KuroCoder <habib.pa98@gmail.com>
2020-09-04 18:32:32 +0430

    add Injections

---
+++ InternshipWeb/Internship.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InternShip.App.Interfaces;
+using InternShip.App.Services;
+using Internship.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Internship.Data.Repository;
+
+namespace Internship.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        //Injections
+        public static void RegisterServices(IServiceCollection service)
+        {
+
+            #region Application Layer Service Injections
+
+            service.AddScoped<IUserService, UserService>();
+
+            #endregion
+
+
+
+            #region Domain Layer Interfaces and Data Layer Repository Injections
+
+            service.AddScoped<IUserRepository, UserRepository>();
+
+            #endregion
+
+        }
+    }
+}
