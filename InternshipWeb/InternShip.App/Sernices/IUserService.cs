5ccdcf608110b3b1df615b22b7dfd8b11d8d01f6
KuroCoder <habib.pa98@gmail.com>
2020-09-04 17:20:24 +0430

    add VM and Interface

---
+++ InternshipWeb/InternShip.App/Sernices/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InternShip.App.ViewModels;
+
+namespace InternShip.App.Sernices
+{
+    public interface IUserService
+    {
+        UserViewModel GetUsers();
+    }
+}
---
+++ InternshipWeb/InternShip.App/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Internship.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternShip.App.ViewModels
+{
+    public class UserViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+    }
+}
